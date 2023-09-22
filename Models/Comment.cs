0eb72d546b038fbfc0b55f9ce5dfcbccad55c403
MohAlfarsi <143397277+MohAlfarsi@users.noreply.github.com>
2023-09-22 13:08:50 +0400

    New added details

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -8,5 +8,8 @@ namespace GitCopy.Models
     public class Comment
     {
         
+        public int Id { get; set; }
+        public String text  { get; set; } = "";
+        
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -7,6 +7,7 @@ namespace GitCopy.Models
 {
     public class Post
     {
-        
+        public int Id { get; set; }
+        public String Title { get; set; } = "Title";
     }
 }
